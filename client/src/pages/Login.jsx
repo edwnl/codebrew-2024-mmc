717844772412c6a59e3681e3472aacbf3a62541d
Yashwanth <its.yashwanth.n@gmail.com>
2024-04-05 09:47:19 +1100

    refactor: redid login page with tailwind css

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,66 +1,29 @@
-import styled from '@emotion/styled';
-import { Button, Container, Grid, Hidden, Typography } from '@mui/material';
+import React from 'react';
+import { Button } from '@mui/material';
 
-export const Login = () => {
+const Login = () => {
   return (
-    <SignUpContainer id="signup-container" maxWidth="fit-content">
-      <Grid container spacing={3}>
-        <Hidden smDown>
-          <ImageContainer item md={6}>
-            {/* TODO: Add Image and update background color for sign up container*/}
-            {/* <Image /> */}
-          </ImageContainer>
-        </Hidden>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 w-screen">
+      <div className="container flex items-center justify-center mx-auto px-4">
+        <div className="hidden md:block md:w-1/2">
+          {/* TODO: Add Image and update background color for sign up container*/}
+          {/* <Image /> */}
+        </div>
 
-        <ContentContainer item xs={12} md={6}>
-          <SignUpBox>
-            <Typography variant="h4" gutterBottom>
-              Welcome to AI Wardrobe
-            </Typography>
+        <div className="w-full md:w-1/2 md:pl-10">
+          {/* Content Section */}
+          <div className="bg-white rounded-lg shadow-md p-8">
+            <h1 className="text-3xl font-semibold mb-4">Welcome to AI Wardrobe</h1>
             {/* TODO: ADD LOGO HERE */}
-            <Typography variant="body1" gutterBottom>
-              Please sign in to continue.
-            </Typography>
-            <Button variant="contained">Sign Up</Button>
-          </SignUpBox>
-        </ContentContainer>
-      </Grid>
-    </SignUpContainer>
+            <p className="text-lg mb-4">Please sign in to continue.</p>
+            <Button variant="contained" className="w-full">
+              Sign Up
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
+
 export default Login;
-
-const SignUpContainer = styled(Container)`
-  background-color: #f0f0f0;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  padding: 0% 10% !important;
-  align-items: center;
-`;
-
-const ImageContainer = styled(Grid)`
-  text-align: center;
-`;
-
-// const Image = styled.img`
-//   max-width: 100%;
-//   max-height: 100%;
-// `;
-
-const ContentContainer = styled(Grid)`
-  text-align: center;
-  align-items: center;
-  justify-content: center;
-`;
-
-const SignUpBox = styled(Container)`
-  display: flex;
-  flex-direction: column;
-  background-color: #fff;
-  min-height: 30vh;
-  align-items: center;
-  justify-content: center;
-  border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-`;
