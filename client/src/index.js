750060086ce2e9b57fe6e6964d817b8cca6b7fda
Ed <edwinli123456@gmail.com>
2024-04-05 02:16:31 +1100

    removed excess files we wont use

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
