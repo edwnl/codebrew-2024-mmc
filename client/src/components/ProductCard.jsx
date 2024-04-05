464bf2fb9c860af90a99c7ecd02a91cc314b4d99
Ed <edwinli123456@gmail.com>
2024-04-05 12:48:01 +1100

    Implemented clothes and fits page

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,16 +1,28 @@
-import { Card } from 'antd';
+import {Card} from 'antd';
 import React from 'react';
+import Tags from "./Tags";
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({product}) => {
   return (
-    <Card
-      hoverable
-      cover={<img alt={product.name} src={product.imageUrl} />}
-      className="max-w-64 m-2"
-    >
-      <Card.Meta title={product.name} description={`AU$${product.price}`} />
-    </Card>
+      <Card
+          hoverable
+          cover={<img alt={product.name} src={product.imageUrl}/>}
+          className="max-w-64 m-2"
+      >
+          <Card.Meta title={product.name}/>
+          <Tags tags={["t-shirt", "red", "blue", "indigo", ]} />
+      </Card>
   );
 };
 
 export default ProductCard;
+// <div id="root">
+//   <nav className="flex flex-col items-center justify-center mx-auto p-4 border-b-2 border-black">
+//     <div className="text-3xl tracking-wider pb-4">WARDROBE</div>
+//   </nav>
+//   <main className="flex bg-white justify-center items-center w-full">
+//     <div className="flex justify-center items-center mt-16">
+//       <div className="max-w-sm p-6 bg-white shadow rounded-lg border-2 border-black"></div>
+//     </div>
+//   </main>
+// </div>
--- client/src/components/ProductGrid.jsx
+++ client/src/components/ProductGrid.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ProductCard from './ProductCard';
 
-const ProductGrid = ({ products }) => {
+const ProductGrid = ({ products, cardComponent }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-start p-8">
-      {products.map((product, index) => (
-        <ProductCard key={index} product={product} />
-      ))}
-    </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-start p-8">
+        {products.map((product, index) => (
+            <div key={index}>
+              {React.createElement(cardComponent, { product })}
+            </div>
+        ))}
+      </div>
   );
 };
 
