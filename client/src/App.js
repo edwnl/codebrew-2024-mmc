3b68fe7ecca98ed4215f1c3f5f3841e4bf670bf2
Ed <edwinli123456@gmail.com>
2024-04-05 11:25:36 +1100

    Replaced old navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
-import styled from '@emotion/styled';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { PAGE_ROUTES } from './utils/pageRoutes';
-import { Box } from '@mui/material';
-import NavigationbarEd from './components/NavigationbarEd';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <NavigationbarEd />
-        <main className="flex bg-white justify-center items-center w-full">
-          <Routes>
-            {PAGE_ROUTES.map((page) => (
-              <Route key={page.key} path={page.pathname} element={<page.component />} />
-            ))}
-          </Routes>
-        </main>
+          <NavBar />
+          <main className="flex bg-white justify-center items-center w-full">
+              <Routes>
+                  {PAGE_ROUTES.map((page) => (
+                      <Route key={page.key} path={page.pathname} element={<page.component />} />
+                  ))}
+              </Routes>
+          </main>
       </BrowserRouter>
     </>
   );
---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = ({ size }) => {
+    return (
+        <div className={`${size} tracking-wider pb-4`}>
+            WARDROBE
+        </div>
+    );
+};
+
+export default Logo;
