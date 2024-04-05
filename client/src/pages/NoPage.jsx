e51eab52a0be81951090babe297b5be5bbb6645c
Yashwanth <its.yashwanth.n@gmail.com>
2024-04-05 23:41:21 +1100

    style: minor css update

--- client/src/pages/NoPage.jsx
+++ client/src/pages/NoPage.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const NoPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <p className="text-2xl mb-5">Oops! Nothing here :(</p>
+      <p className="text-2xl sm:text-4xl mb-5">Oops! Nothing here :(</p>
       <Link to="/" className="text-blue-500 hover:underline">
-        <button className="text-white bg-black hover:bg-gray-800 rounded-lg text-sm px-4 py-2 text-center">
+        <button className="text-white bg-black hover:bg-gray-800 rounded-lg text-sm px-6 py-3">
           Go to Main Page
         </button>
       </Link>
